d5ce3f3cc2cd6e9c472b2c517101153c232c4213
krrish-kohli <krrishkohli@icloud.com>
2026-01-22 18:17:30 +0530

    new files added

---
+++ Simple_Counter_App/script.js
@@ -0,0 +1,34 @@
+// make sure the elements are loaded
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Select elements
+  const counterValue = document.getElementById("counter-value");
+  const increaseBtn = document.getElementById("increase-btn");
+  const decreaseBtn = document.getElementById("decrease-btn");
+  const resetBtn = document.getElementById("reset-btn");
+
+  // Global value
+  let count = 0;
+
+  // add events to the buttons
+  increaseBtn.addEventListener("click", () => {
+    count++;
+    // update the counter value
+    updateCounter();
+  });
+  decreaseBtn.addEventListener("click", () => {
+    count--;
+    // update the counter value
+    updateCounter();
+  });
+  resetBtn.addEventListener("click", () => {
+    count = 0;
+    // update the counter value
+    updateCounter();
+  });
+  // update counter fn
+
+  function updateCounter() {
+    counterValue.textContent = count;
+  }
+});
